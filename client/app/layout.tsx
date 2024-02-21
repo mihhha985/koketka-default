c11ac8847b2763d4133419a885ba2bd5e32fc826
John Doe <johndoe@example.com>
2024-02-21 16:02:46 +0300

    fixed mistake ym

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import { Suspense } from "react";
-import {Metrika} from "@/component/Metrika";
+import Script from "next/script";
 import { 
 	Cormorant_Garamond, 
 	Comfortaa, 
@@ -44,12 +43,24 @@ export default function RootLayout({
     <html lang="en">
 			<head>
 				<link rel="icon" href="/favicon.png"  type="image/png"/>	
-				<link rel="preconnect" href="htts://mc.yandex.ru"/>
 			</head>
       <body className={`${main.variable} ${link.variable} ${logo.variable}`}>
-				<Suspense>
-					<Metrika />
-				</Suspense>
+				<Script id="metrika-counter" strategy="afterInteractive">
+            {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+              ym(96532441, "init", {
+                    defer: true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              });`
+            }
+          </Script>
 				{children}
 			</body>
     </html>
